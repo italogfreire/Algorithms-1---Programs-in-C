77bb1ec936f0776b749b1712aeeccfa0923a9d21
italogfreire <italogfreire987@gmail.com>
2022-06-15 22:57:17 -0300

    positive_or_negative

---
+++ image/README.md
@@ -0,0 +1,7 @@
+<h1>Atividade da disciplina de Algoritmos e Lógica de Programação 2</h1>
+
+<p>Esse é um pequeno programa feito em C, que pede para o usuário 
+   um número qualquer e informa se o número é positivo ou negativo.
+</p>
+
+<img src="./image/image_code.png" alt="image">
--- positive_or_negative.c
+++ positive_or_negative.c
@@ -3,24 +3,19 @@
 #include <locale.h>
 
 int main(){
-	setlocale(LC_ALL,"Portuguese");
+	
+	setlocale(LC_ALL, "Portuguese");
 	
 	int numero;
-	
-	printf("Digite um número inteiro: \n");
+	printf("Digite um número: \n");
 	scanf("%d", &numero);
 	
 	if(numero > 0){
-	
-	
-		printf("O número é positivo. \n" );
-	}
-	
-	else if(numero < 10){
-		printf("O número é negativo. \n");
-	}
-	
-	else{
-			printf("O número é igual a 0 \n");
-	}
+		printf("O número é positivo \n");
+	}else{
+		if(numero < 0)
+			printf("O número é negativo \n");
+		else
+			printf("O númerto é igual a 0");
+	}	
 }
